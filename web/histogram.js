cb2e0866f2cc7aa6b8de987b38d0970ce8274b31
David Hahn <david.hahn@tinkermill.org>
2018-06-02 19:52:02 -0600

    Fixed the issue with multiple charts on screen which caused weird issues with mouse-over functions.

--- web/histogram.js
+++ web/histogram.js
@@ -1,13 +1,34 @@
 var stats = {};
+var dataUrl = 'http://tinker-access.s3-website-us-east-1.amazonaws.com/tinker-access-stats.json';
+var barChartData = {
+	labels: ['Midnight', '1am', '2am', '3am', '4am', '5am', '6am', '7am', '8am', '9am', '10am', '11am', 'Noon', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', '9pm', '10pm', '11pm'],
+	datasets: []
+};
+
+
 
 var init = function()
 {
-  url = 'http://tinker-access.s3-website-us-east-1.amazonaws.com/tinker-access-stats.json';
+	var ctx = document.getElementById('histogram').getContext('2d');
+	window.myBar = new Chart(ctx, {
+		type: 'bar',
+		data: barChartData,
+		options: {
+			responsive: true,
+			legend: {
+				position: 'top',
+			},
+			title: {
+				display: true,
+				text: 'Select Tool and Day of Week'
+			}
+		}
+	});
 
   $.ajax(
     {
       method: 'GET',
-      url: url,
+      url: dataUrl,
       success: initSuccess,
       error: initError
     }
@@ -46,30 +67,15 @@ var changeTool = function()
   for( var i = 0; i < 24; i++ )
     shifted[i] = 100 * stats[tool][day][(i+offset)%24];
 
-  var barChartData = {
-    labels: ['Midnight', '1am', '2am', '3am', '4am', '5am', '6am', '7am', '8am', '9am', '10am', '11am', 'noon', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', '9pm', '10pm', '11pm'],
-    datasets: [{
-      label: '% In Use',
-      borderWidth: 1,
-      data: shifted
-    }]
-  };
-
-  var ctx = document.getElementById('histogram').getContext('2d');
-  window.myBar = new Chart(ctx, {
-    type: 'bar',
-    data: barChartData,
-    options: {
-      responsive: true,
-      legend: {
-        position: 'top',
-      },
-      title: {
-        display: true,
-        text: tool + ' usage history on ' + day + 's'
-      }
-    }
-  });
+	var title = tool + ' usage history on ' + day + 's';
+  barChartData.datasets.length = 0;
+	var newDataset = {
+		label: '% In Use',
+		borderWidth: 1,
+		data: shifted
+	};
+	barChartData.datasets.push(newDataset);
+	window.myBar.update();
 };
 
 
